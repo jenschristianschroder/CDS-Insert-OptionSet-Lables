23bb2bcb92a4e83fb992cd2f14a97a2a8d553498
Jens Schrøder <systemadmin@jschroder.onmicrosoft.com>
2020-03-12 09:06:02 +0100

    initial commit

--- InsertOptionSetValue/Program.cs
+++ InsertOptionSetValue/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.IO;
 
 namespace InsertOptionSetValue
 {
@@ -10,6 +10,67 @@ namespace InsertOptionSetValue
     {
         static void Main(string[] args)
         {
+            string optionSetName = "";
+            string csvFile = "";
+            string connectionString = "";
+            int optionValueStart = 0;
+
+            try {
+                if (args.Length != 4)
+                    throw new Exception($"ERROR: Incorrect number of arguments provided.\nExpected 3 arguments and received {args.Length}");
+
+                optionSetName = args[0];
+
+                if (!int.TryParse(args[1], out optionValueStart))
+                    throw new Exception("OptionValueStart must be an Integer");
+
+                csvFile = args[2];
+
+                connectionString = args[3];
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine();
+                Console.WriteLine("Usage: InsertOptionSetValue.exe [OptionSetLogicolName] [OptionValueStart] [File Path] [Organisation Connection String]");
+                Console.ReadKey();
+                return;
+            }
+
+            //connect to CDS
+            CrmServiceClient conn = new CrmServiceClient(connectionString);
+            IOrganizationService orgService = (IOrganizationService)conn.OrganizationServiceProxy;
+
+            int value = optionValueStart;
+
+            //open csv file
+            using (var reader = new StreamReader(csvFile))
+            {
+                //read csv file line by line
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    // Create a request.
+                    InsertOptionValueRequest insertOptionValueRequest =
+                        new InsertOptionValueRequest
+                        {
+                            OptionSetName = optionSetName,
+                            Label = new Label(values[0], 1033),
+                            Value = value
+                        };
+
+                    // Execute the request.
+                    int insertOptionValue = ((InsertOptionValueResponse)orgService.Execute(insertOptionValueRequest)).NewOptionValue;
+
+                    value++;
+
+                    Console.WriteLine("Created {0} with the value of {1}.", insertOptionValueRequest.Label.LocalizedLabels[0].Label, insertOptionValue);
+                }
+            }
+            Console.WriteLine("\n******** COMPLETE ********");
+            Console.WriteLine($"Inserted {value - optionValueStart} values into {optionSetName}");
         }
     }
 }
